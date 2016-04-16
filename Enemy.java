7138aef59387875e529790c81b2332212e404745
moskung111222 <moskung111222@gmail.com>
2016-04-16 18:55:19 +0700

    *03*เคลื่อนซ้ายขวาแก้ใหม่  extra เปลี่ยนรูปตัวละคร
     ปรับปรุง เคลื่อนที่ซ้ายขวาได้  เพิ่มเติม เปลี่ยนรูปให้ของขวัญตกลงมา แล้ว เปลี่ยนรูปตัวละครเป็นผู้ชายแทน โดยการ import java.awt.Toolkit; 
    ให้เอารูปในไฟส์มาแทรกได้

--- Enemy.java
+++ Enemy.java
@@ -5,18 +5,20 @@ package f2.spw;
  import java.awt.AlphaComposite;
  import java.awt.Color;
  import java.awt.Graphics2D;
+ import java.awt.Toolkit; /*import image tool*/
+ import java.awt.Image;
  
  public class Enemy extends Sprite{
  	public static final int Y_TO_FADE = 400;
  	public static final int Y_TO_DIE = 600;
  	
--	private int step = 12;
-+	private int step = 10;
+	private int step = 12;
+	private int step = 10;
  	private boolean alive = true;
  	
  	public Enemy(int x, int y) {
--		super(x, y, 5, 10);
-+		super(x, y, 8, 12);
+		super(x, y, 8, 12);
+		super(x, y, 10, 14);
  		
  	}
  
@@ -28,8 +30,10 @@ package f2.spw;
  			g.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 
  					(float)(Y_TO_DIE - y)/(Y_TO_DIE - Y_TO_FADE)));
  		}
--		g.setColor(Color.GREEN);
-+		g.setColor(Color.RED);
+		g.setColor(Color.GREEN);
+		/*g.setColor(Color.RED);*/
  		g.fillRect(x, y, width, height);
+ 		 Image img = Toolkit.getDefaultToolkit().getImage("teddy.gif");
+ 		 g.drawImage(img, x, y, width, height, null);
  		
  	}
--- SpaceShip.java
+++ SpaceShip.java
@@ -3,30 +3,36 @@
  
  import java.awt.Color;
  import java.awt.Graphics2D;
- 
+ import java.awt.Toolkit;
+ import java.awt.Image;
  public class SpaceShip extends Sprite{
  
  	int step = 8;
  	
  	public SpaceShip(int x, int y, int width, int height) {
  		super(x, y, width, height);
+
+ 		super(x, y, 30,40);
  		
  	}
   
   	@Override
+  	
   	public void draw(Graphics2D g) {
  		g.setColor(Color.GREEN);
- 		g.setColor(Color.BLUE);
+ 		/*g.setColor(Color.BLUE);*/
   		g.fillRect(x, y, width, height);
-  		
+  		Image img = Toolkit.getDefaultToolkit().getImage("man.gif-c200");
+
   	}
   
- 	public void move(int direction){
+ 	/*public void move(int direction){
  		x += (step * direction);
  		if(x < 0)
  			x = 0;
  		if(x > 400 - width)
- 			x = 400 - width;
+ 			x = 400 - width;*/
+ 	
  	public void move(int directionX, int directionY){
  		if(directionX != 0 && directionY == 0 ){
  			x += (step * directionX);
